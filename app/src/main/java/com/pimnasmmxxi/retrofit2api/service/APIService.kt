91931a5a06a285e3e0159c821d3a9f222caed6b2
holysyid <holysyid@gmail.com>
2021-09-09 22:48:45 +0700

    adding header on APIService.kt

--- app/src/main/java/com/pimnasmmxxi/retrofit2api/service/APIService.kt
+++ app/src/main/java/com/pimnasmmxxi/retrofit2api/service/APIService.kt
@@ -3,8 +3,17 @@ package com.pimnasmmxxi.retrofit2api.service
 import com.pimnasmmxxi.retrofit2api.model.ResponseResult
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface APIService {
+
+    @Headers(
+        "Accept:*/*",
+        "Accept-Encoding:gzip, deflate, br",
+        "Content-Type:application/json",
+        "Connection:keep-alive"
+    )
+
     @GET("/api/v1/food-comps")
     suspend fun getDataNested(): Response<ResponseResult>
 }
